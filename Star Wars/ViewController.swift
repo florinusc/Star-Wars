dc61f096184833a94dd13ebf86a0ec690b76b59f
Florin Uscatu <florinusc@gmail.com>
2020-01-14 17:47:18 +0200

    Initial Commit

---
+++ Star Wars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Star Wars
+//
+//  Created by Florin Uscatu on 14/01/2020.
+//  Copyright © 2020 Florin Uscatu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
