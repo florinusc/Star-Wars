6cfea23a24e90715f464aebef1ba805297f197f0
Florin Uscatu <florinusc@gmail.com>
2020-01-16 19:27:52 +0200

    Add Common framework for holding common helpers
    Refactor Data Layer into separate framework

---
+++ Common/Common/Helpers/CustomError.swift
@@ -0,0 +1,23 @@
+//
+//  CustomError.swift
+//  DataLayer
+//
+//  Created by Florin Uscatu on 15/01/2020.
+//  Copyright © 2020 Florin Uscatu. All rights reserved.
+//
+
+import Foundation
+
+public enum CustomError: Error {
+    case general
+    case network
+}
+
+extension CustomError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .general: return NSLocalizedString("Something went wrong", comment: "General Error")
+        case .network: return NSLocalizedString("There was a problem with the network", comment: "Network Error")
+        }
+    }
+}
