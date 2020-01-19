daacdd4608ac73816ead1ca2539c10355c22fe1e
Florin Uscatu <florinusc@gmail.com>
2020-01-19 19:54:22 +0200

    Add UI tests

--- Star WarsUITests/Star_WarsUITests.swift
+++ Star WarsUITests/Star_WarsUITests.swift
@@ -16,13 +16,32 @@ class Star_WarsUITests: XCTestCase {
         continueAfterFailure = false
         app = XCUIApplication()
         app.launchArguments.append("--uitesting")
+        app.launch()
+    }
+    
+    func test_correctNamesAppearInCells_inMainGrid() {
+        XCTAssertTrue(app.collectionViews.cells.element(boundBy: 0).staticTexts["Luke"].exists)
+        XCTAssertTrue(app.collectionViews.cells.element(boundBy: 1).staticTexts["Yoda"].exists)
+    }
+    
+    func test_mainGridViewAppears_inMainGridScreen() {
+        XCTAssertTrue(app.otherElements["mainGrid"].exists)
     }
     
     func test_openingCorrectDetailScreen_afterMainGridCellTap() {
-        app.launch()
-        XCTAssertTrue(app.otherElements["mainGrid"].exists)
         app.collectionViews.cells.element(boundBy: 0).tap()
         XCTAssertTrue(app.staticTexts["Luke"].exists)
+    }
+    
+    func test_correctDetailsAppearInDetailScreen_afterMainGridCellTap() {
+        app.collectionViews.cells.element(boundBy: 0).tap()
+        XCTAssertEqual(app.staticTexts["heightLabel"].label, "177 cm")
+        XCTAssertEqual(app.staticTexts["weightLabel"].label, "200 kg")
+        XCTAssertEqual(app.staticTexts["eyeColorLabel"].label, "green")
+        XCTAssertEqual(app.staticTexts["skinColorLabel"].label, "fair")
+        XCTAssertEqual(app.staticTexts["birthYearLabel"].label, "19BBY")
+        XCTAssertEqual(app.staticTexts["hairColorLabel"].label, "brown")
+        XCTAssertEqual(app.staticTexts["genderLabel"].label, "male")
     }
    
 }
